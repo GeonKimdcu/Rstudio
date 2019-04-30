0eb664d760b5844b121cac4466b4a966d2773bdb
GeonKimdcu <geon0599@cu.ac.kr>
2019-04-30 14:16:20 +0900

    chapter07-1

---
+++ Do It! R/07/chap07-1-exam.R
@@ -0,0 +1,14 @@
+#혼자서 해보기
+
+library(dplyr)
+mpg <- as.data.frame(ggplot2::mpg)
+mpg[c(65,124,131,153,212), "hwy"] <- NA
+
+# Q1.
+table(is.na(mpg$drv))
+table(is.na(mpg$hwy))
+
+# Q2.
+mpg %>% filter(!is.na(hwy)) %>% 
+  group_by(drv) %>%
+  summarise(hwy_mean = mean(hwy))
