54d21ae35a62d76cb79550a9de470522ab094c82
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-07 22:24:34 +0900

    chap 08-3 막대 그래프

--- Do It! R/08/chap08-3.R
+++ Do It! R/08/chap08-3.R
@@ -1,4 +1,7 @@
+# 평균 막대 그래프
 library(dplyr)
+library(ggplot2)
+# 1. 집단별 평균표 만들기 
 mpg<-as.data.frame(ggplot2::mpg)
 
 df_mpg <- mpg %>% 
@@ -7,15 +10,22 @@ df_mpg <- mpg %>%
 
 df_mpg
 
+# 2. 그래프 생성하기 
 ggplot(data = df_mpg, aes(x = drv, y = mean_hwy))+
-  geom_col()
+  geom_col()   # 막대 그래프 만드는 함수 geom_col()
 
+# 3. 크기 순으로 정렬하기 
 ggplot(data = df_mpg, aes(x=reorder(drv, -mean_hwy), y =mean_hwy))+
   geom_col()
+# reorder(x축 변수, 정렬 기준으로 삼을 변수지정)    -는 내림차순 없으면 오름차순 
 
+# 빈도 막대 그래프 만들기 
+# 빈도 막대그래프는 y축 없이 지정, geom_bar() 사용
 ggplot(data=mpg, aes(x=drv))+geom_bar()
 
 ggplot(data=mpg, aes(x=hwy))+geom_bar()
+
+# geom_col() 는 요약값을 사용 / geom_bar()는 원자료 사용 
 
 # 혼자서 해보기 
 # Q1.
