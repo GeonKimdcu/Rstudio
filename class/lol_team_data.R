52048ec057c98f00797d68e9f2ba497ea390c865
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-09 21:53:18 +0900

    lol 팀별 승률

--- class/lol_team_data.R
+++ class/lol_team_data.R
@@ -12,3 +12,11 @@ df_team <- df_team[, -c(7:13)]
 names(df_team) <- c("순위", "팀명", "total", "win", "lose", "승률") 
 str(df_team)
 View(df_team)
+
+library(dplyr)
+df_team <- df_team %>% 
+  group_by(팀명) %>% 
+  arrange(desc(승률)) %>% head(5)
+df_team
+library(ggplot2)
+ggplot(data = df_team, aes(x=팀명, y= 승률))+geom_col()
