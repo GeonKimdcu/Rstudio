fc37bc2152afd308760420498b582af6d92cf30d
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-21 13:49:47 +0900

    chap06-6 직업별 월급 차이

--- Do It! R/09/ch09-6.R
+++ Do It! R/09/ch09-6.R
@@ -3,8 +3,8 @@ list_job<- read_excel("Koweps_Codebook.xlsx", col_names = T, sheet =2)
 head(list_job)
 dim(list_job)
 
-library(dplyr)
-welfare <- left_join(welfare, list_job, by = "code_job")
+library(dplyr)     # left_join() 함수를 사용하기 위해선 dplyr 패키지 필요!
+welfare <- left_join(welfare, list_job, by = "code_job")  # by 대신 id사용가능 
 welfare %>% 
   filter(!is.na(code_job)) %>% 
   select(code_job, job) %>% 
@@ -25,7 +25,7 @@ top10
 
 library(ggplot2)
 ggplot(data = top10, aes(x = reorder(job, mean_income), y= mean_income))+geom_col()+
-  coord_flip()
+  coord_flip() # 막대가 회전되었기 때문에 reorder로 오름차순해도 큰값이 맨위로 옴 
 
 bottom10 <- job_income %>% 
   arrange(mean_income) %>% 
@@ -35,6 +35,6 @@ bottom10
 
 ggplot(data= bottom10, aes(x=reorder(job, -mean_income),
                            y= mean_income))+
-  geom_col()+
-  coord_flip()+
+  geom_col()+    # 막대그래프 함수
+  coord_flip()+  # 막대 오른쪽으로 90도 회전 
   ylim(0,850)
